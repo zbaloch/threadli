c0e74b35b6c93f352c292ddc19960daaf6d42dca
Zaheer Baloch <zaheer@hey.com>
2026-03-05 20:04:23 +0300

    Updated the task scheduler timing

--- src/main/java/com/threadli/threadli_web/services/ScheduledTaskService.java
+++ src/main/java/com/threadli/threadli_web/services/ScheduledTaskService.java
@@ -6,7 +6,10 @@ import java.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
 import com.threadli.threadli_web.repositories.ThreadRepository;
@@ -17,34 +20,46 @@ import com.threadli.threadli_web.models.User;
 import com.threadli.threadli_web.models.ThreadMembership;
 
 import java.util.List;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 
 @Service
-public class ScheduledTaskService {
+@EnableScheduling
+public class ScheduledTaskService implements SchedulingConfigurer {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTaskService.class);
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
+    @Value("${threadli.scheduler.email.cron}")
+    private String emailSchedulerCron;
+
     @Autowired
     private UserRepository userRepository;
-    
+
     @Autowired
     private WorkspaceRepository workspaceRepository;
-    
+
     @Autowired
     private ThreadRepository threadRepository;
-    
+
     @Autowired
     private ThreadMembershipRepository threadMembershipRepository;
 
     @Autowired
     private EmailService emailService;
 
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.addTriggerTask(
+            this::sendScheduledNotifications,
+            new CronTrigger(emailSchedulerCron)
+        );
+    }
 
     /**
-     * Example: Send scheduled notifications
+     * Send scheduled email notifications based on cron expression from THREADLI_SCHEDULER_EMAIL_CRON environment variable
+     * Cron format: second minute hour day month weekday
+     * Example: "0 0 9,15 * * *" runs at 9:00am and 3:00pm every day
      */
-    @Scheduled(cron = "0 0 6-18/2 * * *")
-    @org.springframework.context.event.EventListener(org.springframework.boot.context.event.ApplicationReadyEvent.class)
     private void sendScheduledNotifications() {
         log.info("Processing scheduled notifications...");
 
